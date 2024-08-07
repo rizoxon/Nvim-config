4df1ef21b0003f6c0103587537e91bfac8a3c8f5
Muhammadrizo <madaminov.co@gmail.com>
2024-08-07 14:41:46 +0500

    one dark

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,85 +1,40 @@
--- require("gruvbox").setup({
--- 	terminal_colors = true, -- add neovim terminal colors
--- 	undercurl = true,
--- 	underline = true,
--- 	bold = false,
--- 	italic = {
--- 		strings = true,
--- 		emphasis = true,
--- 		comments = true,
--- 		operators = false,
--- 		folds = true,
--- 	},
--- 	strikethrough = true,
--- 	invert_selection = false,
--- 	invert_signs = false,
--- 	invert_tabline = false,
--- 	invert_intend_guides = false,
--- 	inverse = true, -- invert background for search, diffs, statuslines and errors
--- 	contrast = "hard", -- can be "hard", "soft" or empty string
--- 	palette_overrides = {
--- 		bright_green = "#b8bb26",
--- 	},
--- 	overrides = {},
--- 	dim_inactive = false,
--- 	transparent_mode = false,
--- })
--- vim.cmd("colorscheme gruvbox")
+require('onedark').setup  {
+    style = 'warm', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = false,  -- Show/hide background
+    term_colors = true, -- Change terminal color as per the selected theme style
+    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
 
--- require("catppuccin").setup({
---     flavour = "mocha", -- latte, frappe, macchiato, mocha
---     background = { -- :h background
---         light = "latte",
---         dark = "mocha",
---     },
---     transparent_background = false, -- disables setting the background color.
---     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
---     term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
---     dim_inactive = {
---         enabled = false, -- dims the background color of inactive window
---         shade = "dark",
---         percentage = 0.15, -- percentage of the shade to apply to the inactive window
---     },
---     no_italic = false, -- Force no italic
---     no_bold = false, -- Force no bold
---     no_underline = false, -- Force no underline
---     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
---         comments = { "italic" }, -- Change the style of comments
---         conditionals = { "italic" },
---         loops = {"bold"},
---         functions = {"bold"},
---         keywords = {},
---         strings = {},
---         variables = {},
---         numbers = {},
---         booleans = {"italic"},
---         properties = {},
---         types = {},
---         operators = {},
--- --         -- miscs = {}, -- Uncomment to turn off hard-coded styles
---     },
---     color_overrides = {
--- 		mocha = {
--- 			base = "#1E2326"
--- 		},
--- 	},
---     custom_highlights = {},
---     default_integrations = true,
---     integrations = {
---         cmp = true,
---         gitsigns = true,
---         nvimtree = true,
---         treesitter = true,
---         notify = false,
---         mini = {
---             enabled = true,
---             indentscope_color = "",
---         },
--- --         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
---     },
--- })
---
--- -- setup must be called before loading
--- vim.cmd.colorscheme "catppuccin"
+    -- toggle theme style ---
+    toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+    toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
 
-require("everforest").load()
+    -- Change code style ---
+    -- Options are italic, bold, underline, none
+    -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+    code_style = {
+        comments = 'italic',
+        keywords = 'none',
+        functions = 'none',
+        strings = 'none',
+        variables = 'none'
+    },
+
+    -- Lualine options --
+    lualine = {
+        transparent = true, -- lualine center bar transparency
+    },
+
+    -- Custom Highlights --
+    colors = {}, -- Override default colors
+    highlights = {}, -- Override highlight groups
+
+    -- Plugins Config --
+    diagnostics = {
+        darker = true, -- darker colors for diagnostic
+        undercurl = true,   -- use undercurl instead of underline for diagnostics
+        background = true,    -- use background color for virtual text
+    },
+}
+-- Lua
+require('onedark').load()
