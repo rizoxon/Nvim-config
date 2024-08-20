3846a1aadf4c38ce3e33052f078aabba9a074c17
Muhammadrizo <madaminov.co@gmail.com>
2024-08-20 17:49:58 +0500

    keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -16,7 +16,8 @@ vim.opt.listchars = {
 }
 
 vim.opt.list = true
-vim.opt.listchars:append "tab:│ "
+vim.opt.listchars:append "tab:  "
+-- vim.opt.listchars:append "tab:│ "
 vim.opt.expandtab = false
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -39,7 +40,7 @@ vim.opt.backspace = "2"
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 vim.opt.autoread = true
 
 -- tab and spaces
@@ -51,8 +52,8 @@ vim.opt.shiftround = true
 vim.opt.expandtab = false
 
 vim.keymap.set("n", "<leader>cs", ":nohlsearch<CR>")
-vim.keymap.set("n", "<C-s>", ":w<CR>")
-vim.keymap.set("n", "<leader>q", ":q<cr>")
+-- vim.keymap.set("n", "<C-s>", ":w<CR>")
+-- vim.keymap.set("n", "<leader>q", ":q<cr>")
 
 
 vim.keymap.set("n", "x", '"_x')
