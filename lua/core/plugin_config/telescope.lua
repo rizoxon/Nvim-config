9c8776b30a2b69e6babfb7802d56ba99fd05b232
rizoxon <madaminov.co@gmail.com>
2024-12-24 12:01:39 +0300

    some update

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -4,4 +4,8 @@ vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = "Find marks" })
+
 vim.keymap.set('n', '<leader>r', builtin.registers, {})
+
+vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = "Git status" })
