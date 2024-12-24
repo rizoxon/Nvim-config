e98605c6d5bce32d0c9812cb435bb225455d9409
rizoxon <madaminov.co@gmail.com>
2024-12-24 12:17:41 +0300

    design update

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,11 +1,34 @@
+
+require('telescope').setup({
+	defaults = {
+		layout_strategy = "horizontal", -- Change to 'vertical', 'center', or 'cursor'
+		layout_config = {
+			width = 0.8,
+			height = 0.9,
+			prompt_position = "top",
+			horizontal = { preview_width = 0.6, },
+			vertical = { preview_height = 0.4, },
+		},
+	},
+})
+
+local themes = require('telescope.themes')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+vim.keymap.set('n', '<leader><leader>', function()
+	builtin.find_files(themes.get_dropdown({
+		width = 0.5,
+		previewer = false,
+	}))
+end, {})
+
+vim.keymap.set('n', '<leader>fb', function()
+	builtin.buffers(themes.get_dropdown({
+		width = 0.5,
+		previewer = false,
+	}))
+end, {})
+
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = "Find marks" })
-
-vim.keymap.set('n', '<leader>r', builtin.registers, {})
-
 vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = "Git status" })
