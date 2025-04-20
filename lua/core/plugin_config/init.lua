77314a8a8dc651e7033e2bcc943726f568151da3
rizoxon <madaminov.co@gmail.com>
2025-04-20 13:01:11 +0500

    update

--- lua/core/plugin_config/init.lua
+++ lua/core/plugin_config/init.lua
@@ -2,7 +2,6 @@ require("core.plugin_config.lualine")
 require("core.plugin_config.nvim-tree")
 require("core.plugin_config.treesitter")
 require("core.plugin_config.telescope")
-require("core.plugin_config.lsp_config")
 require("core.plugin_config.auto_complete")
 require("core.plugin_config.harpoon")
 require("core.plugin_config.fold")
@@ -10,6 +9,7 @@ require("core.plugin_config.undotree")
 require("core.plugin_config.gitsigns")
 require("core.plugin_config.alpha")
 require("core.plugin_config.spectre")
+require("core.plugin_config.lsp_config")
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
