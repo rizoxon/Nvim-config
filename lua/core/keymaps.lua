f8d9494621467547cd4b105cabbc361ab40e1642
rizoxon <madaminov.co@gmail.com>
2025-04-26 11:44:03 +0500

    some update

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -72,7 +72,7 @@ vim.opt.fileencodings = { "utf-8", "latin1" }
 
 
 -- Flash
-vim.keymap.set({"n", "x", "o"}, "/", function() require("flash").jump() end, { desc = "Flash Search" })
+vim.keymap.set({"n", "x", "o"}, "<C-s>", function() require("flash").jump() end, { desc = "Flash Search" })
 vim.keymap.set({"n", "x", "o"}, "?", function() require("flash").treesitter() end, { desc = "Flash treesitter" })
 
 
